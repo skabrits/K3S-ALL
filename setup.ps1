e1f0dd710f89bbf73331bcf4968c3a36062402bb
sevak <seva.kabrits@gmail.com>
2023-01-10 16:54:13 +0300

    fix

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,8 @@
+netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=80
+netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=8080
+netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=443
+netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=9090
+netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=9099
 netsh advfirewall firewall add rule name= "Open Port 6443 K8S" dir=in action=allow protocol=TCP localport=6443
 netsh advfirewall firewall add rule name= "Open Port 10250 K8S" dir=in action=allow protocol=TCP localport=10250
 netsh advfirewall firewall add rule name= "Open Port 2379 K8S" dir=in action=allow protocol=TCP localport=2379
@@ -15,6 +20,11 @@ wsl --set-default-version 2
 
 $wsl_ip=$(wsl -- ip -o -4 -json addr list eth0 | ConvertFrom-Json | %{ $_.addr_info.local } | ?{ $_ })
 
+netsh interface portproxy add v4tov4 listenport=80 listenaddress=0.0.0.0 connectport=80 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenport=8080 listenaddress=0.0.0.0 connectport=8080 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenport=443 listenaddress=0.0.0.0 connectport=443 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenport=9090 listenaddress=0.0.0.0 connectport=9090 connectaddress=$wsl_ip
+netsh interface portproxy add v4tov4 listenport=9099 listenaddress=0.0.0.0 connectport=9099 connectaddress=$wsl_ip
 netsh interface portproxy add v4tov4 listenport=6443 listenaddress=0.0.0.0 connectport=6443 connectaddress=$wsl_ip
 netsh interface portproxy add v4tov4 listenport=10250 listenaddress=0.0.0.0 connectport=10250 connectaddress=$wsl_ip
 netsh interface portproxy add v4tov4 listenport=2379 listenaddress=0.0.0.0 connectport=2379 connectaddress=$wsl_ip
